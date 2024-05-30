7266ae29276c04b7115d02bbfe51eb24fc2e0f47
gowthamnamuru <gowthamnamuri@gmail.com>
2024-05-30 05:41:03 +0530

    Added useState hook example

--- src/App.js
+++ src/App.js
@@ -6,16 +6,23 @@ import Sidebar from './components/Sidebar';
 import Bag from './components/Bag';
 import Apple from './components/Apple';
 import Pear from './components/Pear';
+import Child from './data-flow/child';
+import InputComponent from './hooks/Inputcomponent';
+import RegistrationForm from './hooks/RegistrationForm';
 
 function App() {
+  const date = new Date()
   return (
-    <div>
-    <Header name = 'Gowtham' color = 'purple'/>
-    <Main greet = 'Howdy'/>
-    <Sidebar greet = 'Howdy'/>
-    <Bag children = {<Apple color = "yellow" number = "5" />}/>
-    <Bag children = {<Pear friend = "Anshu"/>}/>
-    </div>
+    // <div>
+    // <Header name = 'Gowtham' color = 'purple'/>
+    // <Main greet = 'Howdy'/>
+    // <Sidebar greet = 'Howdy'/>
+    // <Bag children = {<Apple color = "yellow" number = "5" />}/>
+    // <Bag children = {<Pear friend = "Anshu"/>}/>
+    // <Child message = {date.toLocaleTimeString()} />
+    // </div>
+    // <InputComponent />
+    <RegistrationForm />
   );
 }
 
